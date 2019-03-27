ff965be55508565f87cb43ea5fc967effaf5fb93
moonwave <moonwave.systems@gmail.com>
2019-03-27 15:59:38 -0700

    add sources

---
+++ j01/src/set-src-links.sh
@@ -0,0 +1,7 @@
+SRC=/home/moonwave/workspace/github/moonwave/lib/j01/src
+cd /j01/src
+ln -s ${SRC}/guava-19.0-sources.jar guava-19.0-sources.jar
+ln -s ${SRC}/glassfish-embedded-all-3.1.2.2-src.jar glassfish-embedded-all-3.1.2.2-src.jar
+ln -s ${SRC}/hadoop-1.2.1-src.jar hadoop-1.2.1-src.jar
+ln -s ${SRC}/mongo-java-driver-3.2.2-src.jar mongo-java-driver-3.2.2-src.jar
+ln -s ${SRC}/jedis-2.9.0-sources.jar jedis-2.9.0-sources.jar
